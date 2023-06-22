de8ca4e7fe5a589ba110132bd89c985a80533a42
Kamil Starmach <kstarmach@outlook.com>
2023-06-22 14:22:19 +0200

    Create 2665-counter-ii.js

---
+++ 2665-counter-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    var curr = init;
+
+    function increment() {
+        return ++curr;
+    }
+
+    function decrement() {
+        return --curr;
+    }
+
+    function reset() {
+        curr = init;
+        return curr;
+    }
+
+    return {
+        increment,
+        decrement,
+        reset
+    }
+
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */

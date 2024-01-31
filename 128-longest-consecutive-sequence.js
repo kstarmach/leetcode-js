a17e0ffd791f84d9c7ee81369fb7754885da50ad
kstarmach <kstarmach@outlook.com>
2024-01-31 18:30:55 +0100

    Create 128-longest-consecutive-sequence.js

--- 128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence.js
@@ -1,3 +1,5 @@
+// O(nlogn)
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -23,4 +25,34 @@ var longestConsecutive = function (nums) {
     }
 
     return Math.max(result, strike);
+};
+
+// O(n)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    nums.sort((a, b) => a - b);
+    let count = 0;
+    let temp = 1;
+    if (nums.length === 0) {
+        return 0;
+    }
+    if (nums.length === 1) {
+        return 1;
+    }
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1] - 1) {
+            temp++;
+            count = Math.max(count, temp);
+        } else if (nums[i] === nums[i + 1]) {
+            count = Math.max(count, temp);
+        } else {
+            temp = 1;
+            count = Math.max(count, temp);
+        }
+    }
+    return count;
 };
